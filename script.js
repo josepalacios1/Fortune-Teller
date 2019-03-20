f08ef69b795ed746f636c030e21f8dd898d410c7
josepalacios1 <43544155+josepalacios1@users.noreply.github.com>
2019-03-20 10:01:45 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$("button").click(function(){
+    var name=$(".nameInput").val();
+    
+    
+    var number=$(".number").val();
+$("p").text("Hello " + name + "Hello" + number);
+});
+  
+    var button=$(".button").val();
+$("p").text("Hello" + button);
